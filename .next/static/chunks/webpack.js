ccdcdc624e374717980f9b164f86b30032d23b81
Sunnie Kapar <kaparsunnie@gmail.com>
2024-03-22 21:35:58 -0400

    Hot fix with logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ac6ae7e1e59f618"; }
+/******/ 		__webpack_require__.h = function() { return "3b7f3c5419b1cf22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
