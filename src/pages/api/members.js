83a61ffbb4948351c16fd58ecf7df46c949c5bfd
Patrick He <77468352+he-patrick@users.noreply.github.com>
2024-03-29 22:43:04 -0400

    Update members.js

--- src/pages/api/members.js
+++ src/pages/api/members.js
@@ -37,4 +37,10 @@ export const members = [
     year: 2028,
     siteURL: 'https://www.adonojha.com',
   },
+  {
+    name: 'Patrick He',
+    cohort: 'Computer Engineering',
+    year: 2028,
+    siteURL: 'https://patrickhe.com',
+  },
 ];
