9377fa7b2bbbfcdfd60bc1c9d139a386bfce49ef
Roozbeh Ali <roozbeh.ali.2005@gmail.com>
2025-02-13 23:58:54 -0500

    add analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import { MemberProvider } from "@/context/MemberContext";
+import { Analytics } from "@vercel/analytics/react";  
 
 export default function App({ Component, pageProps }) {
   return (
     <MemberProvider>
       <Component {...pageProps} />
+      <Analytics />
     </MemberProvider>
   );
 }
